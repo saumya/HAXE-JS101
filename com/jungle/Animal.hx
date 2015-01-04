ef83b05cd2ae7bd75e408150f618ff396355a990
saumya <2saumyaray@gmail.com>
2015-01-05 01:14:38 +0530

    example setup for HAXE JS

---
+++ com/jungle/Animal.hx
@@ -0,0 +1,23 @@
+/*
+* By : Saumya
+* Example created to work with JS on HAXE
+*/
+package com.jungle;
+// An animal
+// @:expose("myLib.Animal")
+class Animal 
+{
+	private var type:String;
+    private var age:Float;
+    public function new(type:String,age:Float=0.0){
+        trace('Animal:Constructor');
+        this.type = type;
+        this.age = age;
+    }
+    public function getType():String{
+        return this.type;
+    }
+    public function getAge():Float{
+        return this.age;
+    }
+}
